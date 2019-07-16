d54729fb8be1e4044583a7bfdbaf4362d71f3a6c
Harpreet <harpreet.impinge@gmail.com>
2019-07-16 13:44:21 +0530

    SignIn completed with token and signup completed, file uploading in progress

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,70 +9,86 @@
     var userExport = {};
 
     userExport.signUp = async function (req, res) {
-        const userEmailAddress = (req.body.email).toLowerCase();
-        var checkEmail = await isUserExist(userEmailAddress);
-//        console.log(checkEmail, '<<--checkEmail')
-//        if (checkEmail && checkEmail.data != null) {
-//            res.status(400).send(JSON.stringify({"status": false, "error": 'Email Address (' + userEmailAddress + ') already exist.'}));
-//        } else {
-        var userData = {
-            email: userEmailAddress,
-            description: req.body.description,
-            stage: req.body.stage,
-            start_date: req.body.start_date,
-            logo: req.body.logo,
-            team_name: req.body.team_name,
-            memebers: req.body.memebers,
-            role: startupConfig.roles[req.body.role]
-        }
-        if (req.body.password != null && req.body.password != 'null') {
-            var hashedPassword = passwordHash.generate(req.body.password);
-            userData['password'] = hashedPassword;
-        }
-        new User(userData).save(function (error, userResponse) {
-            if (error) {
-                res.status(400).send(JSON.stringify({"status": false, "error": error}));
+        try {
+            const userEmailAddress = (req.body.email).toLowerCase();
+            var checkEmail = await isUserEmailExist(userEmailAddress);
+            if (checkEmail != null && checkEmail.data != null) {
+                res.status(400).send({"status": false, "error": 'Email Address (' + userEmailAddress + ') already exist.'});
             } else {
-                var token = jwt.sign({email: req.body.email}, config.secret, {expiresIn: '24h'});
-                res.status(200).send(JSON.stringify({status: true, "userData": userResponse, "access_token": token}));
-            }
-        });
-//        }
-    }
-    userExport.signIn = async function (req, res) {
-        const userEmailAddress = (req.body.email).toLowerCase();
-        const userResponse = await isUserExist(userEmailAddress);
-        if (userResponse && userResponse.data == null) {
-            res.status(400).send(JSON.stringify({"status": false, "error": 'Email Address (' + userEmailAddress + ') not found. Please sign up.'}));
-        } else {
-            const selectedFields = {
-                'team_name': 1,
-                'role': 1,
-                'email': 1,
-                'description': 1,
-                'stage': 1,
-                'start_date': 1
-            }
-            var checkPassword = passwordHash.verify(req.body.password, userResponse.password);
-            if (checkPassword !== true) {
-                res.status(403).send(JSON.stringify({status: false, error: 'Authentication failed. Wrong password.'}));
-            } else {
-                User.findOne({email: userEmailAddress}).select(selectedFields).exec(function (error, userdata) {
-                    var token = jwt.sign({email: userEmailAddress}, config.secret, {expiresIn: '24h'});
-                    res.status(200).send(JSON.stringify({status: true, "userData": userdata, "access_token": token}));
+                var userData = {
+                    email: userEmailAddress,
+                    description: req.body.description,
+                    stage: req.body.stage,
+                    start_date: req.body.start_date,
+                    logo: req.body.logo,
+                    team_name: req.body.team_name,
+                    memebers: req.body.memebers,
+                    role: startupConfig.roles[req.body.role]
+                }
+                if (req.body.password != null && req.body.password != 'null') {
+                    var hashedPassword = passwordHash.generate(req.body.password);
+                    userData['password'] = hashedPassword;
+                }
+                new User(userData).save(function (error, userResponse) {
+                    if (error) {
+                        res.status(400).send({"status": false, "error": error});
+                    } else {
+                        var token = jwt.sign({email: req.body.email}, config.secret, {expiresIn: '24h'});
+                        res.status(200).send({status: true, "data": userResponse, "access_token": token});
+                    }
                 });
             }
+        } catch (error) {
+            res.status(400).send(JSON.stringify({"status": false, "error": error}));
         }
     }
-
-    function isUserExist(email) {
-        return new Promise((reject, resolve) => {
-            User.findOne({'email': email}, function (error, response) {
-                if (!error) {
-                    resolve(error, response);
+    userExport.signIn = async function (req, res) {
+        try {
+            const userEmailAddress = (req.body.email).toLowerCase();
+            var userResponse = await isUserEmailExist(userEmailAddress);
+            if (userResponse != null && userResponse.data == null) {
+                res.status(400).send({"status": false, "error": 'Email Address (' + userEmailAddress + ') not found. Please sign up.'});
+            } else {
+                const selectedFields = {
+                    '_id': userResponse.data._id,
+                    'team_name': userResponse.data.team_name,
+                    'role': userResponse.data.role,
+                    'email': userResponse.data.email,
+                    'description': userResponse.data.description,
+                    'stage': userResponse.data.stage,
+                    'start_date': userResponse.data.start_date
+                }
+                var checkPassword = passwordHash.verify(req.body.password, userResponse.data.password);
+                if (checkPassword !== true) {
+                    res.status(403).send({status: false, error: 'Authentication failed. Wrong password.'});
                 } else {
-                    console.log(response, 'response')
-                    resolve(null, response)
+                    var token = jwt.sign({email: userEmailAddress}, config.secret, {expiresIn: '24h'});
+                    res.status(200).send({status: true, "data": selectedFields, "access_token": token});
+                }
+            }
+        } catch (error) {
+            res.status(400).send(JSON.stringify({"status": false, "error": error}));
+        }
+    }
+  
+    function isUserEmailExist(email) {
+        return new Promise((resolve) => {
+            User.findOne({'email': email}, function (error, response) {
+                if (error) {
+                    resolve({data: null});
+                } else {
+                    resolve({data: response})
+                }
+            });
+        })
+    }
+    function isUserIdExist(userId) {
+        return new Promise((resolve) => {
+            User.findOne({'_id': userId}, function (error, response) {
+                if (error) {
+                    resolve({data: null});
+                } else {
+                    resolve({data: response})
                 }
             });
         })
