669bd40c5b4fa04a6cc72ec2d10781cb4d2073e2
Harpreet <harpreet.impinge@gmail.com>
2019-07-24 19:30:00 +0530

    role based auth removed

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,7 +9,7 @@ router.post('/signup', userController.signUp);
 router.post('/signin', userController.signIn);
 router.post('/uploadImage',authMiddleware, userController.uploadImage);
 router.post('/deleteImage',authMiddleware, userController.deleteImage);
-router.get('/profile/:id', authMiddleware, roleMiddleware, userController.getProfile);
-router.put('/editprofile/:id', authMiddleware, roleMiddleware, userController.editProfile);
+router.get('/profile/:id', authMiddleware, userController.getProfile);
+router.put('/editprofile/:id', authMiddleware, userController.editProfile);
 
 module.exports = router;
